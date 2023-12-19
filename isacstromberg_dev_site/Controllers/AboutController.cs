d8ac3d3b132e337d749f331fe820cb559e8e0c94
Isac Strömberg <isacstromberg@hotmail.com>
2023-12-19 20:26:12 +0100

    Add project files.

---
+++ isacstromberg_dev_site/Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace isacstromberg_dev_site.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
