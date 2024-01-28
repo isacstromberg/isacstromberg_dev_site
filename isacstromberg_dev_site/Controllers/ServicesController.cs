e8751005e2fa275afb69e6dab3cd03184ffdc8f6
Isac Strömberg <isacstromberg@hotmail.com>
2024-01-28 22:57:47 +0100

    Services page added, need to think a little bit about the design and what the next step is.. I want to add some more cool features make the website living full with interactivity!!

---
+++ isacstromberg_dev_site/Controllers/ServicesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace isacstromberg_dev_site.Controllers
+{
+    public class ServicesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
