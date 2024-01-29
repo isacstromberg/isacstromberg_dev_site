560f50e05e6ca98538344eb5632da643dbfb1184
Isac Strömberg <isacstromberg@hotmail.com>
2024-01-29 22:05:54 +0100

    MVP finished, trying to find more ideas on what to improve just to make the website extra flashy

---
+++ isacstromberg_dev_site/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace isacstromberg_dev_site.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
